7f7763c13d498eb3b13c47b5280735e98e2e190e
mdrobot7 <55768773+mdrobot7@users.noreply.github.com>
2021-09-16 14:40:16 -0500

    algorithm pseudocode
    this is the pseudocode for the algorithm, now change this into python code

--- main.py
+++ main.py
@@ -27,7 +27,30 @@ except FileNotFoundError:
 word = sys.argv[1] #the word to be anagrammed
 word = word.replace(" ", "") #get rid of all of the spaces
 
+#pseudocode for the algorithm:
 
+#in alpha order first. start with the first letter of word, and start indexing through the dict file. let's call the current line 'line'
+#check each line for that char, and stop if it has it.
+#if it stops (has the first char): change to checking line against word.
+#take the first char of LINE, and check if WORD has it. in anagrams, letters can't be repeated, so if word has the letter, remove it from word temporarily.
+#index through the rest of line, and keep checking of word has the letters.
+
+#if it is ever FALSE, that means that the current line has a char that the word doesn't have, so it CAN'T be an anagram.
+#reset the temp (letter-removed) word to the original
+#go back to the beginning of the loop, grab a new line, and start testing the first char again
+
+#if it is TRUE (meaning that all of the letters in line are in word, so it CAN be a part of the anagram):
+#save the line (or at least the line number) in a variable
+#rerun the loop, but with the trimmed down word this time (without the letters of the first saved line)
+
+# - if at least one line is saved, BUT the trimmed down 'word' can't make any other words in the dictionary:
+#move the original saved line (or line number) to a "do not use" variable
+#reset word
+#run the entire loop again, but skip over any lines in the "do not use" variable
+
+#if the program takes all of the letters out of 'word' (meaning that it found a complete anagram, and 'word' is now empty):
+#print out the saved words in order
+#reset the word, run the entire loop over again to find another one (if the user/args specify)
 
 
 
